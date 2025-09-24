dd07a5fa14198cb834fd5316a25449e82350e1f4
natalie <nga3052@g.rit.edu>
2025-09-24 13:34:58 +0400

    hello

---
+++ Desktop/Python/mock.py
@@ -0,0 +1,31 @@
+numb=int(input("enter a number:"))
+def even_odd():
+    """this function is print if the number is even or odd"""
+    #MOCK
+    if numb%2==0:
+        print("number is even")  
+    elif numb%2!=0:
+         print("number is odd")
+   
+even_odd()
+def posi_negative():
+    """THIS FUNCTION IS TO PRINT POSITIVE OR NEGATIVE NUMBER"""
+    #MOCK
+    if numb>=0:
+        print("number is positive")
+    elif numb<=0:
+         print("number is negative")    
+
+posi_negative()
+
+def square():
+    """this function is to find the square of the number"""   
+    #MOCK
+    print(numb**2)
+square()    
+
+def cube():
+    """this function is to find the cube of the number"""
+    #MOCK   
+    print(numb**3)
+cube()    
